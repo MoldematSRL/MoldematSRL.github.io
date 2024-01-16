820b3b40f25efdc68cfea79335f4e38c854b5e12
JuanC <jbolattitorcasio@frba.utn.edu.ar>
2024-01-16 16:10:24 -0300

    Performance changes

--- js/map.js
+++ js/map.js
@@ -1,21 +1,23 @@
-// map.js
-function initMap() {
-    // LeaftletJS Map
-    var map = L.map('map').setView([-34.7217851, -58.3744177], 16);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-    }).addTo(map);
-    var marker = L.marker([-34.7217851, -58.3744177]).addTo(map);
-    marker.bindPopup("<b class='leafletb'>Direccion</b><br>Las Piedras 2804").openPopup();
-    map.touchZoom.disable();
-    map.doubleClickZoom.disable();
-    map.scrollWheelZoom.disable();
-    map.boxZoom.disable();
-    map.keyboard.disable();
-}
+window.addEventListener('load', function () {
+    // map.js
+    function initMap() {
+        // LeaftletJS Map
+        var map = L.map('map').setView([-34.7217851, -58.3744177], 16);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(map);
+        var marker = L.marker([-34.7217851, -58.3744177]).addTo(map);
+        marker.bindPopup("<b class='leafletb'>Direccion</b><br>Las Piedras 2804").openPopup();
+        map.touchZoom.disable();
+        map.doubleClickZoom.disable();
+        map.scrollWheelZoom.disable();
+        map.boxZoom.disable();
+        map.keyboard.disable();
+    }
 
-// Llama a la función initMap en cada página donde quieras mostrar el mapa
-if (document.getElementById('map')) {
-    initMap();
-}
+    // Llama a la función initMap en cada página donde quieras mostrar el mapa
+    if (document.getElementById('map')) {
+        initMap();
+    }
+});
